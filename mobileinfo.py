e8b0bf60efdb24ad50d420dcbdb0c90bf6a1c3b4
AshishBidve <89291762+AshishBidve@users.noreply.github.com>
2021-10-01 23:30:30 +0530

    Add files via upload

---
+++ mobileinfo.py
@@ -0,0 +1,26 @@
+[
+{
+   "id":"1",
+   "company":"apple",
+   "modle":"iphone13 pro",
+   "featurs":"https://www.apple.com/in/iphone-13-pro/",
+   "purchase link":"https://www.amazon.in/dp/B09G9FPGTN",
+   },
+
+   {
+   "id":"2",
+   "company": "oneplus",
+   "modle":"nord2 5G",
+   "featurs":"https://www.oneplus.in/oneplus-nord-2-5g?utm_source=google_smart&utm_medium=smart_cpc&utm_campaign=OnePlus_Nord_2&utm_term=%7Bkeyword%7D&gclsrc=aw.ds&gclid=CjwKCAjw49qKBhAoEiwAHQVTo3uvtqjzaHFVF7JEE56OvfR06HjKeF0ewkm2zdRoXW6e9KCvgZ2mdRoCZIYQAvD_BwE",
+   "purchase link":"https://www.amazon.in/OnePlus-Green-Woods-256GB-Storage/dp/B097RF4WCM/ref=asc_df_B097RF4WCM/?tag=googleshopdes-21&linkCode=df0&hvadid=396988715505&hvpos=&hvnetw=g&hvrand=3002054636824384797&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9062092&hvtargid=pla-1439779935084&psc=1&ext_vrnc=hi",
+   },
+
+   {
+   "id":"3",
+   "company": "realme",
+   "modle":"realme GT 5G ",
+   "featurs":"https://gadgets.ndtv.com/realme-gt-5g-price-in-india-100486",
+   "purchase link":"https://www.amazon.in/Realme-GT-Master-Voyager-Storage/dp/B09DCHYWK7"
+   },
+
+]
