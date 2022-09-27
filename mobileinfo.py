19fe784ec7c0b5ef158032ee8d035c797fc3c3d7
Omkardeokar <89266020+Omkardeokar@users.noreply.github.com>
2022-09-27 12:01:15 +0530

    Update mobileinfo.py

--- mobileinfo.py
+++ mobileinfo.py
@@ -14,6 +14,7 @@
    "featurs":"https://www.oneplus.in/oneplus-nord-2-5g?utm_source=google_smart&utm_medium=smart_cpc&utm_campaign=OnePlus_Nord_2&utm_term=%7Bkeyword%7D&gclsrc=aw.ds&gclid=CjwKCAjw49qKBhAoEiwAHQVTo3uvtqjzaHFVF7JEE56OvfR06HjKeF0ewkm2zdRoXW6e9KCvgZ2mdRoCZIYQAvD_BwE",
    "purchase link":"https://www.amazon.in/OnePlus-Green-Woods-256GB-Storage/dp/B097RF4WCM/ref=asc_df_B097RF4WCM/?tag=googleshopdes-21&linkCode=df0&hvadid=396988715505&hvpos=&hvnetw=g&hvrand=3002054636824384797&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9062092&hvtargid=pla-1439779935084&psc=1&ext_vrnc=hi",
    },
+   
 
    {
    "id":"3",
