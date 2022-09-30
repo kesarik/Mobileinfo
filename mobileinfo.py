99daf127e56aad57a3713e78589ab180ee9329d6
Ishika Kaur Sandhu <113432816+Ishikakaur30@users.noreply.github.com>
2022-09-30 22:50:22 +0530

    added redmi note 10 pro max mobile

--- mobileinfo.py
+++ mobileinfo.py
@@ -164,6 +164,14 @@
    "featurs":"https:https://www.mi.com/in/redmi-11-prime/",
    "purchase link":"https://www.mi.com/in/redmi-11-prime/"
    }  
+ {
+   
+    "id":"100",
+   "company": "redmi",
+   "modle":"redmi note 10 pro max",
+   "featurs":"High end device",
+   "purchase link":"https://amazon.in/redmi_note10_pro_max-f-41"
+   },
 
 
 
